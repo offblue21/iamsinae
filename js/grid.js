9db9ab37805638c8673448deb6d3199ff4b15e21
offblue21 <sl3797@columbia.edu>
2016-04-28 11:33:54 -0400

    Added files via upload

---
+++ js/grid.js
@@ -0,0 +1,32 @@
+Highcharts.theme = {
+    colors: ['#999999', '#cecece', '#1e9ba5', '#23b5c9', '#24CBE5', '#64E572', 
+             '#FF9655', '#FFF263', '#6AF9C4'],
+    chart: {
+        backgroundColor: '#FFFFFF',
+    },
+    title: {
+        style: {
+            color: '#666666',
+            font: '16px'
+        }
+    },
+    subtitle: {
+        style: {
+            color: '#666666',
+            font: '12px'
+        }
+    },
+
+    legend: {
+        itemStyle: {
+            font: '9pt',
+            color: '#666666'
+        },
+        itemHoverStyle:{
+            color: '#666666'
+        }   
+    }
+};
+
+// Apply the theme
+var highchartsOptions = Highcharts.setOptions(Highcharts.theme);
